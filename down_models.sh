cac2bcfd79b1a7682b7080fc025342930e152bef
s13bby <qaws3623@gmail.com>
2026-02-22 22:16:41 +0600

    update
    Updated API key placeholders for Civitai downloads.

--- down_models.sh
+++ down_models.sh
@@ -2,11 +2,11 @@
 curl -L "https://huggingface.co/NSFW-API/NSFW-Wan-UMT5-XXL/resolve/main/nsfw_wan_umt5-xxl_fp8_scaled.safetensors" \
   --output "models/clip/nsfw_wan_umt5-xxl_fp8_scaled.safetensors"
 
-curl -f -L -H "Authorization: Bearer 0a2b67b2730688c953517b659a9f61d5" \
+curl -f -L -H "Authorization: Bearer YOUR_API_KEY_FOR_CIVITAI" \
   "https://civitai.com/api/download/models/2467166?type=Model&format=GGUF&size=full&fp=fp16" \
   --output "models/unet/DasiwaWAN22I2V14BTastysinV8_q5High.gguf"
 
-curl -f -L -H "Authorization: Bearer 0a2b67b2730688c953517b659a9f61d5" \
+curl -f -L -H "Authorization: Bearer YOUR_API_KEY_FOR_CIVITAI" \
   "https://civitai.com/api/download/models/2467350?type=Model&format=GGUF&size=full&fp=fp16" \
   --output "models/unet/DasiwaWAN22I2V14BTastysinV8_q5Low.gguf"
 
